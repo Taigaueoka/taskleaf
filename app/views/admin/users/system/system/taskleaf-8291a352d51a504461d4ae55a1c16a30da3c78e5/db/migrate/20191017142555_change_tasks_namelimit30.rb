f5fd28c1b16dd77122e49720b4a1f39edb567a2d
EC2 Default User <ec2-user@ip-172-31-40-222.us-east-2.compute.internal>
2019-10-18 08:53:35 +0000

    ‘最初のコミット’1018

--- app/views/admin/users/system/system/taskleaf-8291a352d51a504461d4ae55a1c16a30da3c78e5/db/migrate/20191017142555_change_tasks_namelimit30.rb
+++
@@ -1,8 +0,0 @@
-class ChangeTasksNamelimit30 < ActiveRecord::Migration[5.2]
-  def update
-    change_column :tasks, :name, :string, limit: 30
-  end
-  def down
-    change_column :tasks, :name, :string
-  end
-end
